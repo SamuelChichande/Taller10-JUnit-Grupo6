21e55618e0efd4540c344eb12b923f6ae40e77e1
SamuelChichande <sjchicha@espol.edu.ec>
2025-07-30 13:28:56 -0500

    Add files via upload

---
+++ Taller10-JUnit-Grupo6/src/main/java/com/espol/taller10/junit/grupo6/Taller10JUnitGrupo6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espol.taller10.junit.grupo6;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller10JUnitGrupo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
